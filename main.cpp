f603289b58770d89e7fb4d8935fc747da6b61538
stone <11911611@mail.sustech.edu.cn>
2021-01-02 15:13:30 +0800

    haha

--- main.cpp
+++ main.cpp
@@ -11,7 +11,20 @@ using namespace cv;
 int main() {
 
     Mat image = imread("/Users/stone/CLionProjects/SimpleCNNbyCPP/samples/face.jpg");
-    cout<<image.channels();
+    Mat Channels[3];
+    split(image, Channels);//BGR
+    float *r=new float [128*128*3];//RGB
+    for(int i=0;i<128*128;i++){
+        r[i]=Channels[2].data[i]/255;
+    }
+    for(int i=0;i<128*128;i++){
+        r[i+128*128]=Channels[1].data[i]/255;
+    }
+    for(int i=0;i<128*128;i++){
+        r[i+128*128*2]=Channels[0].data[i]/255;
+    }
+
     return 0;
 }
 
+void maxpool(float* data,)
